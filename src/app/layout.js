0f0e9e5bb7a76d30974a3143f279bc1cb017df1d
dreamacademy-id <kikiamaliaaa725@gmail.com>
2024-09-24 01:46:50 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,12 @@ export const metadata = {
   title: 'Dream Academy',
   description: 'Dream Academy adalah web tryout pertama di Indonesia Timur',
   keywords: 'dreamacademy, tryout, sbmptn',
-  openGraph: {
-    title: 'Dream Academy Tryout',
-    description: 'Dream Academy adalah web tryout pertama di Indonesia Timur',
-    url: 'https://dreamacademy.id',
-    type: 'website',
-  },
+  // openGraph: {
+  //   title: 'Dream Academy Tryout',
+  //   description: 'Dream Academy adalah web tryout pertama di Indonesia Timur',
+  //   url: 'https://dreamacademy.id',
+  //   type: 'website',
+  // },
 };
 
 import Head from "next/head";
@@ -21,31 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <title>Dream Academy</title>
-        <meta name="description" content="Dream Academy adalah web tryout pertama di Indonesia Timur" />
-        <meta name="keywords" content="dreamacademy, tryout, sbmptn" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="Dream Academy Tryout" />
-        <meta property="og:description" content="Dream Academy adalah web tryout pertama di Indonesia Timur" />
-        <meta property="og:url" content="https://dreamacademy.id" />
-        <meta property="og:type" content="website" />
         <link rel="icon" href="/logoDa.jpg" />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              "name": "Dream Academy",
-              "url": "https://dreamacademy.id",
-              "logo": "https://dreamacademy.id/logo.png",
-              "sameAs": [
-                "https://www.facebook.com/dreamacademy",
-                "https://www.instagram.com/dreamacademy"
-              ]
-            }),
-          }}
-        />
       </Head>
       <body>
         <AuthProvider>
